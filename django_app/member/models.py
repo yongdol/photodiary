e0920e3a8388071bd3862a351f4c0fb8e32280fc
yongdol <kungbuk77@gmail.com>
2016-12-01 03:41:12 +0900

    make member default model

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+
+class MyUserManager(UserManager):
+    pass
+
+
+class MyUser(AbstractUser):
+    pass
--- django_app/photodiary/settings.py
+++ django_app/photodiary/settings.py
@@ -102,7 +102,7 @@ else:
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
